0aa8310be06a78248ee4264e6b35ff9331446cd9
Vardhan, Gyan <vardhang@arcesium.com>
2021-03-19 18:43:38 +0530

    log at Fri Mar 19 18:43:37 2021

--- fake_commit.py
+++ fake_commit.py
@@ -3,7 +3,7 @@ import subprocess as sp, datetime
 
 def make_changes_and_commit(times=1):
     for _ in range(times):
-        to_append = 'log at ' + str(datetime.datetime.today().ctime()) + '\"'
+        to_append = 'log at ' + str(datetime.datetime.today().ctime())
         file = open('dummy.txt', 'a')
         file.write(to_append + '\n')
         file.close()
@@ -11,4 +11,9 @@ def make_changes_and_commit(times=1):
         print(sp.run(['git', 'commit', '-m', to_append]))
 
 
-make_changes_and_commit(1)
+def push_origin():
+    print(sp.run(['git', 'push', 'origin', 'master']))
+
+
+make_changes_and_commit(21)
+push_origin()
