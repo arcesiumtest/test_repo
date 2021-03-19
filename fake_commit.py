d61fd35604445def61ff17402b09470201445bba
Vardhan, Gyan <vardhang@arcesium.com>
2021-03-19 18:41:21 +0530

    log at Fri Mar 19 18:41:21 2021"

--- fake_commit.py
+++ fake_commit.py
@@ -5,7 +5,7 @@ def make_changes_and_commit(times=1):
     for _ in range(times):
         to_append = 'log at ' + str(datetime.datetime.today().ctime()) + '\"'
         file = open('dummy.txt', 'a')
-        file.write(to_append)
+        file.write(to_append + '\n')
         file.close()
         print(sp.run(['git', 'add', '*']))
         print(sp.run(['git', 'commit', '-m', to_append]))
